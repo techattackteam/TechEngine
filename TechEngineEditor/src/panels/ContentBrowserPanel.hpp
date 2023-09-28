31ea4193e287a1ef5cf5c69f751359ad8b8441d6
mmigas100 <miguel.al.faria@gmail.com>
2023-09-28 17:21:39 +0100

    feat: content browser left side folder hierarchy

--- TechEngineEditor/src/panels/ContentBrowserPanel.hpp
+++ TechEngineEditor/src/panels/ContentBrowserPanel.hpp
@@ -4,12 +4,12 @@
 #include "Panel.hpp"
 
 namespace TechEngine {
-    class ProjectBrowserPanel : public Panel {
+    class ContentBrowserPanel : public Panel {
     private:
         std::filesystem::path currentPath;
         bool menuWindowOpen = false;
     public:
-        ProjectBrowserPanel();
+        ContentBrowserPanel();
 
         void init();
 
@@ -20,5 +20,9 @@ namespace TechEngine {
         void renameFile(const std::string &filename, const std::string &newName);
 
         void deleteFile(const std::string &filename);
+
+        bool Splitter(bool split_vertically, float thickness, float *size1, float *size2, float min_size1, float min_size2, float splitter_long_axis_size);
+
+        void RenderSceneHierarchy(const std::filesystem::path &directoryPath);
     };
 }
