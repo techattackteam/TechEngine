b8eda2315610eaf7c1fee6805f16e42a01e13c01
Miguel Faria <miguel.al.faria@gmail.com>
2024-01-28 23:05:49 +0000

    feat: TechEngineAPI dummy library

---
+++ TechEngineAPI/src/scene/SceneManagerAPI.cpp
@@ -0,0 +1,9 @@
+#include "SceneManagerAPI.hpp"
+#include "scene/SceneManager.hpp"
+
+
+namespace TechEngineAPI {
+    void SceneManager::loadScene() {
+        sceneManager->loadScene("placeholder");
+    }
+}
