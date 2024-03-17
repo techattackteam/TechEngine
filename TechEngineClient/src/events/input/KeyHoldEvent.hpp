4f2383490237e25bd53577cba64498ffeef9a88f
Miguel Faria <miguel.al.faria@gmail.com>
2024-03-17 13:20:53 +0000

    feat: EventDispatcherAPI

--- TechEngineClient/src/events/input/KeyHoldEvent.hpp
+++ TechEngineClient/src/events/input/KeyHoldEvent.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
 #include "core/Key.hpp"
+#include "event/events/Event.hpp"
 
 namespace TechEngine {
-    class KeyHoldEvent : public TechEngine::Event {
+    class KeyHoldEvent : public Event {
     private:
         Key key;
+
     public:
         inline static EventType eventType = EventType("KeyHoldEvent", EventTiming::ASYNC);
 
         KeyHoldEvent(Key key) : key(key), Event(eventType) {
-
         };
 
-        Key &getKey() {
+        Key& getKey() {
             return key;
         };
     };
--- TechEngineClient/src/scriptingAPI/event/EventDispatcherAPI.cpp
+++ TechEngineClient/src/scriptingAPI/event/EventDispatcherAPI.cpp
@@ -2,11 +2,15 @@
 #include "event/EventDispatcher.hpp"
 
 namespace TechEngine {
-    EventDispatcherAPI::EventDispatcherAPI(TechEngine::EventDispatcher* eventDispatcher) {
+    EventDispatcherAPI::EventDispatcherAPI(EventDispatcher* eventDispatcher) {
         EventDispatcherAPI::eventDispatcher = eventDispatcher;
     }
 
     void EventDispatcherAPI::dispatch(Event* event) {
         eventDispatcher->dispatch(event);
+    }
+
+    void EventDispatcherAPI::subscribe(EventType eventType, const std::function<void(Event*)>& callback) {
+        eventDispatcher->subscribe(eventType, callback);
     }
 }
