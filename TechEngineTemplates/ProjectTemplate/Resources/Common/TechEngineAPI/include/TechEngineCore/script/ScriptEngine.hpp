2b80b75b4bbb627b73195daf67971dfaba55da1c
Miguel <miguel.al.faria@gmail.com>
2024-05-29 22:26:21 +0100

    reverted glfw as submodule

---
+++ TechEngineTemplates/ProjectTemplate/Resources/Common/TechEngineAPI/include/TechEngineCore/script/ScriptEngine.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <list>
+#include <filesystem>
+#include <windows.h>
+
+#include "Script.hpp"
+#include "eventSystem/EventDispatcher.hpp"
+
+namespace TechEngine {
+    class ScriptEngine {
+    private:
+        std::list<Script*> scripts = {};
+        bool runtime = false;
+        bool dllLoaded = false;
+        bool loadingScripts = false;
+
+        HINSTANCE m_userCustomDll = nullptr;
+        HANDLE m_dllProcessHandle = nullptr;
+        HANDLE m_dllThreadHandle = nullptr;
+
+        EventDispatcher* eventDispatcher = nullptr;
+
+    public:
+        explicit ScriptEngine(bool runtime);
+
+        void init(const std::string& dllPath, EventDispatcher* eventDispatcher);
+
+        void onStart();
+
+        void onUpdate();
+
+        void onFixedUpdate();
+
+        Script* getScript(const std::string& name);
+
+        void registerScript(Script* script);
+
+        void deleteScripts();
+
+        void stop();
+
+        bool isLoadingScripts() const {
+            return loadingScripts;
+        }
+    };
+}
