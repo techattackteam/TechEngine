2c43ca69ff3b76955a03cb07170e6b5e3e1140eb
Miguel <miguel.al.faria@gmail.com>
2024-08-18 20:03:10 +0100

    Project Manager create and export project

--- engine/core/src/project/ProjectManager.hpp
+++ engine/core/src/project/ProjectManager.hpp
@@ -3,10 +3,19 @@
 #include <filesystem>
 
 namespace TechEngine {
+    enum class ProjectType {
+        CLIENT,
+        SERVER,
+        EDITOR
+    };
+
     class CORE_DLL ProjectManager : public System {
     private:
         std::filesystem::path m_projectPath;
         std::filesystem::path m_projectName;
+        const std::filesystem::path m_runtimesPath = std::filesystem::current_path().string() + "\\runtimes";
+        const std::filesystem::path m_clientRuntimePath = m_runtimesPath.string() + "\\client";
+        const std::filesystem::path m_serverRuntimesPath = m_runtimesPath.string() + "\\server";
 
     public:
         explicit ProjectManager(const std::filesystem::path& projectPath);
@@ -15,7 +24,9 @@ namespace TechEngine {
 
         void shutdown() override;
 
-        void exportProject(const std::filesystem::path& path);
+        void exportProject(const std::filesystem::path& path, ProjectType type);
+
+        void createProject(const std::string& projectName);
 
     private:
         void createDefaultProject();
