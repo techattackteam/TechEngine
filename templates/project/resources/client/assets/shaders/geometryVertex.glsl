804d41585a06ec249d6ed26c85270519f9097f2f
Miguel Faria <miguel.al.faria@gmail.com>
2025-10-01 20:11:31 +0100

    Fix final dot product NdotL using the View instead of light view. Added radius and intensity properties do point light

--- templates/project/resources/client/assets/shaders/geometryVertex.glsl
+++ templates/project/resources/client/assets/shaders/geometryVertex.glsl
@@ -17,23 +17,22 @@ struct ObjectData {
     uint materialID;
 };
 
-
 layout (std430, binding = 1) buffer ObjectBuffer {
     ObjectData objects[];
-} objectBuffer;
+};
 
 
 uniform bool isLightingActive;
 void main() {
     int objectIndex = gl_BaseInstance + gl_InstanceID;;
 
-    mat4 model = objectBuffer.objects[objectIndex].modelMatrix;
+    mat4 model = objects[objectIndex].modelMatrix;
     vec4 worldPos = model * vec4(position, 1.0f);
     v_worldPos = worldPos.xyz;
 
     v_normal = mat3(model) * normal;
 
-    f_materialID = objectBuffer.objects[objectIndex].materialID;
+    f_materialID = objects[objectIndex].materialID;
 
     gl_Position = u_projection * u_view * model * vec4(position, 1.0f);
 }
