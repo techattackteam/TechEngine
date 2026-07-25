57513ea5933e9a53f459f53b5237960affb52dde
Miguel Faria <miguel.al.faria@gmail.com>
2026-07-25 19:27:15 +0100

    Made debug/release context aware logger

--- apps/editor/src/main.cpp
+++ apps/editor/src/main.cpp
@@ -8,5 +8,10 @@ int main() {
     int x = 10;
     int y = -10;
     TE_LOGGER_INFO("Starting TechEngine Editor: {1}, {0}", x, y);
+    TE_LOGGER_WARN("Starting TechEngine Editor: {1}, {0}", x, y);
+    TE_LOGGER_ERROR("Starting TechEngine Editor: {1}, {0}", x, y);
+    TE_LOGGER_CRITICAL("Starting TechEngine Editor: {1}, {0}", x, y);
+    TE_LOGGER_DEBUG("Starting TechEngine Editor: {1}, {0}", x, y);
+    TE_LOGGER_TRACE("Starting TechEngine Editor: {1}, {0}", x, y);
     return TechEngine::run();
 }
