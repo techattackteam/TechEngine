4daf96cd3363a680bd4463749a50383ca82c0ef3
Miguel Faria <miguel.al.faria@gmail.com>
2026-08-03 18:59:39 +0100

    Fix test compile

--- engine/base/tests/diagnostics/AssertTests.cpp
+++ engine/base/tests/diagnostics/AssertTests.cpp
@@ -1,7 +1,7 @@
 #include <TechEngine/base/diagnostics/Assert.hpp>
 #include <TechEngine/base/diagnostics/Log.hpp>
 
-#include "FormatBuffer.hpp"
+#include "diagnostics/FormatBuffer.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 #include <array>
--- engine/base/tests/diagnostics/LogTests.cpp
+++ engine/base/tests/diagnostics/LogTests.cpp
@@ -1,6 +1,6 @@
 #include <TechEngine/base/diagnostics/Log.hpp>
 
-#include "FormatBuffer.hpp"
+#include "diagnostics/FormatBuffer.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 #include <array>
