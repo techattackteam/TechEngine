4512024d42f95380a885b26a70de84bb7d2e2492
Miguel Faria <miguel.al.faria@gmail.com>
2026-09-05 23:08:51 +0100

    Fixed the project dev path (#73)

--- apps/editor/src/main.cpp
+++ apps/editor/src/main.cpp
@@ -5,7 +5,7 @@
 #include <filesystem>
 
 int main(int argc, char** argv) {
-    const std::filesystem::path projectRoot = argc > 1 ? argv[1] : "projects/dev";
+    const std::filesystem::path projectRoot = argc > 1 ? argv[1] : TE_EDITOR_DEFAULT_PROJECT_ROOT;
 
     return TechEngine::runApp<TechEngine::EditorApp>(projectRoot);
 }
